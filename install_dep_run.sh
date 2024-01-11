6cad0f1c8fcdc81524cc0e60e4f660ff0fc39ecc
Ethan <tylercemer@gmail.com>
2024-01-11 15:10:06 -0600

    *add risc0 toolchain to installation script

--- install_dep_run.sh
+++ install_dep_run.sh
@@ -21,6 +21,18 @@ then
     all_dependencies_installed=false
 fi
 
+# Install Risc0 toolchain
+
+#check if cargo-risczero is installed
+if ! command -v cargo-risczero &> /dev/null
+then
+    echo "risczero toolchain not found, installing ..."
+    cargo install cargo-binstall
+    cargo binstall cargo-risczero
+    cargo risczero install
+    all_dependencies_installed=false
+fi
+
 # Check Scarb
 if ! command -v scarb &> /dev/null
 then

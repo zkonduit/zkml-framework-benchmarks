26ed47f534aa99ae6b3ca9e24ed43c588cd4e7f6
Ethan <tylercemer@gmail.com>
2024-01-15 17:56:21 -0600

    *revert to commit #28 ae64b03

--- install_dep_run.sh
+++ install_dep_run.sh
@@ -3,38 +3,36 @@
 # Initialize a flag to track if all dependencies are already installed
 all_dependencies_installed=true
 
-# Function to install pyenv
+# Install pyenv
 install_pyenv() {
     echo "Installing pyenv..."
     curl https://pyenv.run | bash
+}
 
-    # Add pyenv to path and initialize immediately after installation
+# Install Python 3.9 using pyenv
+setup_python_env() {
+    echo "Setting up Python 3.9 environment..."
+    pyenv install 3.9
+    pyenv local 3.9
+    python -m venv .env
+    source .env/bin/activate
+    echo "Python 3.9 environment setup complete. Run $ deactivate to deactivate the virtual environment. Run $ source .env/bin/activate to activate the virtual environment."
+}
+
+install_python() {
+    # Install pyenv
+    install_pyenv
+
+    # Add pyenv to path
     export PATH="$HOME/.pyenv/bin:$PATH"
     eval "$(pyenv init --path)"
     eval "$(pyenv virtualenv-init -)"
-}
 
-# Function to setup Python environment
-setup_python_env() {
-    echo "Setting up Python 3.9 environment..."
-    sudo apt-get update
-    yes | sudo apt-get install libbz2-dev libsqlite3-dev liblzma-dev
-    pyenv uninstall 3.9.18
-    pyenv install 3.9.18
-    pyenv local 3.9.18
-    python -m venv .env
-    source .env/bin/activate
-    echo "Python 3.9 environment setup complete. Run 'deactivate' to deactivate the virtual environment. Run 'source .env/bin/activate' to activate the virtual environment."
-}
-
-# Main installation function
-install_python() {
-    install_pyenv
+    # Setup Python environment
     setup_python_env
 }
 
-
-# Check if pyenv is installed, if not, install everything
+# Check if pyenv is installed and setup python 3.9
 if ! command -v pyenv &> /dev/null
 then
     all_dependencies_installed=false
@@ -43,26 +41,6 @@ else
     echo "pyenv is already installed. Setting up a Python 3.9 environment for the folder..."
     all_dependencies_installed=false
     setup_python_env
-fi
-
-# Install jq
-if ! command -v jq &> /dev/null
-then
-    echo "jq not found, installing jq..."
-    yes | sudo apt-get install jq
-    all_dependencies_installed=false
-else
-    echo "jq is already installed."
-fi
-
-# Install SQLite3
-if ! command -v sqlite3 &> /dev/null
-then
-    echo "sqlite3 not found, installing sqlite3..."
-    yes | sudo apt-get install sqlite3
-    all_dependencies_installed=false
-else
-    echo "sqlite3 is already installed."
 fi
 
 if rustup component list --installed | grep -q rust-src; then

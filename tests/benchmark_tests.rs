3eeffe39c1e67c7c6a9b035ddee2609799351e6c
Ethan <tylercemer@gmail.com>
2024-01-15 08:33:37 -0600

    64 core runner.

--- tests/benchmark_tests.rs
+++ tests/benchmark_tests.rs
@@ -100,11 +100,9 @@ mod benchmarking_tests {
                         // we need to run the risc0 zkVM VM on the host to get the proving time
                         run_risc0_zk_vm(test);
                         // pretty print the benchmarks.json file
-                        if test == TESTS[TESTS.len() - 1] {
-                            let benchmarks_json = std::fs::read_to_string("./benchmarks.json").unwrap();
-                            let benchmarks_json: serde_json::Value = serde_json::from_str(&benchmarks_json).unwrap();
-                            println!("{}", serde_json::to_string_pretty(&benchmarks_json).unwrap());
-                        }
+                        let benchmarks_json = std::fs::read_to_string("./benchmarks.json").unwrap();
+                        let benchmarks_json: serde_json::Value = serde_json::from_str(&benchmarks_json).unwrap();
+                        println!("{}", serde_json::to_string_pretty(&benchmarks_json).unwrap());
                     }
                 });
             }
